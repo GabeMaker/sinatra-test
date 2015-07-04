82577076f726c6169333206138e8144cea2b209c
gabe <GabeMaker@users.noreply.github.com>
2015-07-04 13:08:35 +0100

    add views

--- hello.rb
+++ hello.rb
@@ -1,20 +1,9 @@
 require 'sinatra'
 
 get '/' do
-  "
-  <head>
-    <title>Sinatra Test</title
-  </head>
-  <body>
-    <h1>Sinatra Test</h1>
-    <div>
-      <img src='http://www.sinatrarb.com/images/logo.png'>
-    </div>
-  </body>
-  "
+  erb :index
 end
 
 get '/secret' do
-  'This is a secret page'
-  'On the fly changes - thanks to shotgun!'
+  erb :secret
 end
